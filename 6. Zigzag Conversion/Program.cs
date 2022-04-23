03e6a17e60dc508bc36e02b5e2e09c94f98b88a5
robindz <39600642+robindz@users.noreply.github.com>
2022-04-23 23:37:44 +0200

    6. Zigzag Conversion

---
+++ 6. Zigzag Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿using _6._Zigzag_Conversion;
+using Xunit;
+
+string result = Solution.Convert("PAYPALISHIRING", 3);
+Assert.Equal("PAHNAPLSIIGYIR", result);
+
+result = Solution.Convert("PAYPALISHIRING", 4);
+Assert.Equal("PINALSIGYAHRPI", result);
+
+Console.ReadKey();
