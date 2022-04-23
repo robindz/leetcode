67c6951d2eeb116780347cbc56fd8a6be9206106
robindz <39600642+robindz@users.noreply.github.com>
2022-04-23 23:52:25 +0200

    7. Reverse Integer

---
+++ 7. Reverse Integer/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _7._Reverse_Integer
+{
+    public class Solution
+    {
+        public static int Reverse(int x)
+        {
+            int result = 0;
+
+            while (x != 0)
+            {
+                // Get the last number from the original integer.
+                int tail = x % 10;
+
+                // Append the last number of the original integer to the new integer.
+                int temp = result * 10 + tail;
+
+                // Check for overflow, this happens if, after appending the last number
+                // of the original integer to the new integer, the reverse operation
+                // does not yield te same result.
+                //
+                // Basic arithmetic:
+                // temp = result * 10 + tail
+                // temp - tail = result * 10
+                // (temp - tail) / 10 = result
+                if ((temp - tail) / 10 != result)
+                    return 0;
+
+                // Save the new number.
+                result = temp;
+
+                // Remove the last number of the original integer.
+                x /= 10;
+            }
+
+            return result;
+        }
+    }
+}
