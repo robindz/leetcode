b22662c864e8928d19d367d5c209a807ed03c9f9
robindz <39600642+robindz@users.noreply.github.com>
2022-04-24 21:31:51 +0200

    9. Palindrome Number

---
+++ 0009. Palindrome Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using _0009._Palindrome_Number;
+using Xunit;
+
+bool isPalindrome = Solution.IsPalindrome(121);
+Assert.True(isPalindrome);
+
+isPalindrome = Solution.IsPalindrome(-121);
+Assert.False(isPalindrome);
+
+isPalindrome = Solution.IsPalindrome(10);
+Assert.False(isPalindrome);
+
+Console.ReadKey();
---
+++ 0009. Palindrome Number/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace _0009._Palindrome_Number
+{
+    public class Solution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            int reversed = 0, remainder, original = x;
+            while (x != 0)
+            {
+                remainder = x % 10;
+                reversed = reversed * 10 + remainder;
+                x /= 10;
+            }
+
+            return reversed == original;
+        }
+    }
+}
