3c6cdd26793b41c13d34d76d013232f618fe4d68
robindz <39600642+robindz@users.noreply.github.com>
2022-04-27 19:10:50 +0200

    10. Regular Expression Matching

---
+++ 0010. Regular Expression Matching/Program.cs
@@ -0,0 +1,31 @@
+﻿using _0010._Regular_Expression_Matching;
+using Xunit;
+
+bool isMatch = Solution.IsMatch("aa", "a");
+Assert.False(isMatch);
+
+isMatch = Solution.IsMatch("aa", "a*");
+Assert.True(isMatch);
+
+isMatch = Solution.IsMatch("ab", ".*");
+Assert.True(isMatch);
+
+isMatch = Solution.IsMatch("aab", "c*a*b*");
+Assert.True(isMatch);
+
+isMatch = Solution.IsMatch("mississippi", "mis*is*p*.");
+Assert.False(isMatch);
+
+isMatch = Solution.IsMatch("ab", ".*c");
+Assert.False(isMatch);
+
+isMatch = Solution.IsMatch("aaa", "aaaa");
+Assert.False(isMatch);
+
+isMatch = Solution.IsMatch("aab", "c*a*b");
+Assert.True(isMatch);
+
+isMatch = Solution.IsMatch("aaa", "a*a");
+Assert.True(isMatch);
+
+Console.ReadKey();
---
+++ 0010. Regular Expression Matching/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace _0010._Regular_Expression_Matching
+{
+    public class Solution
+    {
+        public static bool IsMatch(string s, string p)
+        {
+            bool[,] memory = new bool[s.Length + 1, p.Length + 1];
+            memory[s.Length, p.Length] = true;
+
+            for (int i = s.Length; i >= 0; i--)
+            {
+                for (int j = p.Length - 1; j >= 0; j--)
+                {
+                    bool matches = i < s.Length && (p[j] == s[i] || p[j] == '.');
+                    if (j + 1 < p.Length && p[j + 1] == '*')
+                    {
+                        memory[i, j] = memory[i, j + 2] || matches && memory[i + 1, j];
+                    }
+                    else
+                    {
+                        memory[i, j] = matches && memory[i + 1, j + 1];
+                    }
+                }
+            }
+
+            return memory[0, 0];
+        }
+    }
+}
