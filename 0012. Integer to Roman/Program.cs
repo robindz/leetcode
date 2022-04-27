c22a759f4478ece263d0b1170ff1c517f69d4804
robindz <39600642+robindz@users.noreply.github.com>
2022-04-27 19:38:07 +0200

    0012. Integer to Roman

---
+++ 0012. Integer to Roman/Program.cs
@@ -0,0 +1,13 @@
+﻿using _0012._Integer_to_Roman;
+using Xunit;
+
+string roman = Solution.IntToRoman(3);
+Assert.Equal("III", roman);
+
+roman = Solution.IntToRoman(58);
+Assert.Equal("LVIII", roman);
+
+roman = Solution.IntToRoman(1994);
+Assert.Equal("MCMXCIV", roman);
+
+Console.ReadKey();
---
+++ 0012. Integer to Roman/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _0012._Integer_to_Roman
+{
+    public class Solution
+    {
+        public static string IntToRoman(int num)
+        {
+            if (num < 0 || num > 3999) return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            string[] romanCharacters = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] numericValues = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            int i = 0;
+            while (num > 0)
+            {
+                while (num >= numericValues[i])
+                {
+                    sb.Append(romanCharacters[i]);
+                    num -= numericValues[i];
+                }
+                i++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
