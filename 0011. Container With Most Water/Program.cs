f11a5bf69528ff653699a54eafdbcd10b4099681
robindz <39600642+robindz@users.noreply.github.com>
2022-04-27 19:24:06 +0200

    0011. Container With Most Water

---
+++ 0011. Container With Most Water/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using _0011._Container_With_Most_Water;
+using Xunit;
+
+int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+int area = Solution.MaxArea(height);
+Assert.Equal(49, area);
+
+height = new int[] { 1, 1 };
+area = Solution.MaxArea(height);
+Assert.Equal(1, area);
+
+Console.ReadKey();
---
+++ 0011. Container With Most Water/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace _0011._Container_With_Most_Water
+{
+    public class Solution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int l = 0, r = height.Length - 1;
+            int water = 0;
+
+            while (l < r)
+            {
+                water = Math.Max(water, (r - l) * Math.Min(height[l], height[r]));
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+
+            return water;
+        }
+    }
+}
