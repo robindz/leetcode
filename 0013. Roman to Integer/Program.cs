276768aa3bb311c1b2672f02980e104bc776870e
robindz <39600642+robindz@users.noreply.github.com>
2022-04-29 23:18:27 +0200

    0013. Roman to Integer

---
+++ 0013. Roman to Integer/Program.cs
@@ -0,0 +1,16 @@
+﻿using _0013._Roman_to_Integer;
+using Xunit;
+
+var number = Solution.RomanToInt("III");
+Assert.Equal(3, number);
+
+number = Solution.RomanToInt("LVIII");
+Assert.Equal(58, number);
+
+number = Solution.RomanToInt("MCMXCIV");
+Assert.Equal(1994, number);
+
+number = Solution.RomanToInt("DCXXI");
+Assert.Equal(621, number);
+
+Console.ReadKey();
---
+++ 0013. Roman to Integer/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0013._Roman_to_Integer
+{
+    public class Solution
+    {
+        public static int RomanToInt(string s)
+        {
+            int num = 0;
+            int previousNumber = 0;
+            Dictionary<char, int> romanNumerals = new Dictionary<char, int>
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 },
+            };
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                char c = s[i];
+                if (romanNumerals[c] >= previousNumber)
+                {
+                    num += romanNumerals[c];
+                }
+                else
+                {
+                    num -= romanNumerals[c];
+                }
+                previousNumber = romanNumerals[c];
+            }
+
+            return num;
+        }
+    }
+}
