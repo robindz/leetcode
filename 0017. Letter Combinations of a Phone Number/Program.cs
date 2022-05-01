7f67d37894cf26e427fb350beb5bcbd233f574d7
robindz <39600642+robindz@users.noreply.github.com>
2022-05-01 20:03:33 +0200

    0017. Letter Combinations of a Phone Number

---
+++ 0017. Letter Combinations of a Phone Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using _0017._Letter_Combinations_of_a_Phone_Number;
+using Xunit;
+
+var combinations = Solution.LetterCombinations("23");
+combinations.Sort();
+Assert.Equal(new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }, combinations);
+
+combinations = Solution.LetterCombinations("");
+combinations.Sort();
+Assert.Equal(new List<string>(), combinations);
+
+combinations = Solution.LetterCombinations("2");
+combinations.Sort();
+Assert.Equal(new List<string> { "a", "b", "c" }, combinations);
+
+Console.ReadKey();
---
+++ 0017. Letter Combinations of a Phone Number/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _0017._Letter_Combinations_of_a_Phone_Number
+{
+    public class Solution
+    {
+        public static List<string> LetterCombinations(string digits)
+        {
+            // Use a FIFO queue
+            Queue<string> combinations = new Queue<string>();
+            if (digits.Length == 0) return new List<string>();
+
+            string[] map = new string[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            combinations.Enqueue(string.Empty);
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int mapIndex = digits[i] - '0' - 2;
+                int count = combinations.Count; // Number of nodes already in the queue       
+                for (int k = 0; k < count; k++)
+                {
+                    string partialCombination = combinations.Dequeue();
+                    foreach (char c in map[mapIndex])
+                        combinations.Enqueue(partialCombination + c);
+                }
+            }
+
+            return combinations.ToList();
+        }
+    }
+}
