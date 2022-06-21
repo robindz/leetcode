ad580c0e2983bf4b09939b1238ed09f793e8890c
robindz <39600642+robindz@users.noreply.github.com>
2022-06-21 21:20:44 +0200

    22. Generate Parentheses

---
+++ 0022. Generate Parentheses/Program.cs
@@ -0,0 +1,16 @@
+﻿using _0022._Generate_Parentheses;
+using Xunit;
+
+var result = Solution.GenerateParenthesis(1);
+Assert.Equal(result.Count, 1);
+Assert.Equal(result[0], "()");
+
+result = Solution.GenerateParenthesis(3);
+Assert.Equal(result.Count, 5);
+Assert.Equal(result[0], "((()))");
+Assert.Equal(result[1], "(()())");
+Assert.Equal(result[2], "(())()");
+Assert.Equal(result[3], "()(())");
+Assert.Equal(result[4], "()()()");
+
+Console.ReadKey();
---
+++ 0022. Generate Parentheses/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace _0022._Generate_Parentheses
+{
+    public class Solution
+    {
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> result = new List<string>();
+            char[] parenthesisArray = new char[n * 2];
+            Backtrack(result, parenthesisArray, 0, 0, 0, n);
+            return result;
+        }
+
+        private static void Backtrack(IList<string> result, char[] parenthesisArray, int index, int openCount, int closedCount, int n)
+        {
+            if (openCount == n && closedCount == n)
+            {
+                result.Add(new string(parenthesisArray));
+                return;
+            }
+
+            if (openCount < n)
+            {
+                parenthesisArray[index] = '(';
+                Backtrack(result, parenthesisArray, index + 1, openCount + 1, closedCount, n);
+            }
+
+            if (closedCount < openCount)
+            {
+                parenthesisArray[index] = ')';
+                Backtrack(result, parenthesisArray, index + 1, openCount, closedCount + 1, n);
+            }
+        }
+    }
+}
