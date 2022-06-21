6201fe28c755320b38062fa5d120f2ab83953247
robindz <39600642+robindz@users.noreply.github.com>
2022-06-21 20:38:47 +0200

    0020. Valid Parentheses

---
+++ 0020. Valid Parentheses/Program.cs
@@ -0,0 +1,9 @@
+﻿using _0020._Valid_Parentheses;
+using Xunit;
+
+Assert.True(Solution.IsValid("()"));
+Assert.True(Solution.IsValid("()[]{}"));
+Assert.False(Solution.IsValid("(]"));
+Assert.False(Solution.IsValid("(("));
+
+Console.ReadKey();
---
+++ 0020. Valid Parentheses/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace _0020._Valid_Parentheses
+{
+    public class Solution
+    {
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 != 0) return false;
+
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(')
+                    stack.Push(')');
+                else if (c == '[')
+                    stack.Push(']');
+                else if (c == '{')
+                    stack.Push('}');
+                else if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
